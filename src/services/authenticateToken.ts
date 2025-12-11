e0ea8e574e0215da06ee49aee195eace8b92398c
DraT0x <felixdupras@gmail.com>
2025-12-10 21:56:36 -0500

    Exclusion du get vinyles de l'exigence du token

--- src/services/authenticateToken.ts
+++ src/services/authenticateToken.ts
@@ -1,8 +1,8 @@
 // Erreur de build fixé par Claude.AI - 2025-12-10
-import jwt from 'jsonwebtoken';
-import { Response, Request, NextFunction } from 'express';
-import HttpStatusCodes from '@src/common/constants/HttpStatusCodes';
-import ENV from '@src/common/constants/ENV';
+import jwt from "jsonwebtoken";
+import { Response, Request, NextFunction } from "express";
+import HttpStatusCodes from "@src/common/constants/HttpStatusCodes";
+import ENV from "@src/common/constants/ENV";
 
 /**
  * Intergiciel pour authentifier le jeton de l'utilisateur
@@ -14,27 +14,28 @@ import ENV from '@src/common/constants/ENV';
  */
 function authenticateToken(req: Request, res: Response, next: NextFunction) {
   // Ne pas vérifier le token si l'url est celui de generatetoken
-  const urlParts = req.url.split('/');
+  const urlParts = req.url.split("/");
   const lastPartOfUrl = urlParts[urlParts.length - 1];
-  if (lastPartOfUrl === 'generatetoken') {
+  if (lastPartOfUrl === "generatetoken") {
     next();
     return;
   }
 
+  if (req.method === 'GET') {
+    next();
+    return;
+  }
+  
   const authHeader = req.headers.authorization;
-  const token = authHeader?.split(' ')[1];
+  const token = authHeader?.split(" ")[1];
 
   if (token == null) return res.sendStatus(HttpStatusCodes.UNAUTHORIZED);
 
-  jwt.verify(
-    token,
-    ENV.Jwtsecret,
-    (err: jwt.VerifyErrors | null) => {
-      if (err) return res.sendStatus(HttpStatusCodes.FORBIDDEN);
+  jwt.verify(token, ENV.Jwtsecret, (err: jwt.VerifyErrors | null) => {
+    if (err) return res.sendStatus(HttpStatusCodes.FORBIDDEN);
 
-      next();
-    },
-  );
+    next();
+  });
 }
 
 export default authenticateToken;
